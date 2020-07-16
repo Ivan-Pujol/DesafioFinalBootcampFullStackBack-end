3447fa9457ef1afe454b262a70f0857339a84c9b
ivan.pujol <ivan.ivanovich@hotmail.com>
2020-07-15 21:52:58 -0300

    adding client folder back to project

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function App() {
+  return (<div className='container'>
+    <h1>Desafio Final do Bootcamp Full Stack</h1>
+    <form action="">
+      <h2>Controle Financeiro Pessoal</h2>
+      <select>
+        <option value="teste" root="teste">teste</option>
+      </select>
+      <div className='row' id='.FourElements'>
+        <span>lançamentos:</span>
+        <span>Receitas:</span>
+        <span>Despesas:</span>
+        <span>Saldo:</span>
+      </div>
+    </form>
+
+  </div>);
+}
